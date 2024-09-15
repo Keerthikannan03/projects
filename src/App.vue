e2735ed02fc8da5ef059f9073accd6b6f2e33e4b
Keerthi <dkeerthi@techkovan.com>
2024-09-15 18:00:18 +0530

    todolist code updated

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HomepageCom/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomepageCom from './components/HomepageCom.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HomepageCom
   }
 }
 </script>
@@ -17,10 +16,10 @@ export default {
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
+  /* -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px; */
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
 
-createApp(App).mount('#app')
+createApp(App)
+.use(vuetify)
+.mount('#app')
